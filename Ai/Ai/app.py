606232338572eda50f29bd46bd7b160cc5007cec
Mohammad Hafez Najafi <151014739+Hafez-CS@users.noreply.github.com>
2024-01-31 13:31:39 +0330

    Add files via upload

---
+++ Ai/Ai/app.py
@@ -0,0 +1,34 @@
+
+"""
+import requests as rq
+
+a = rq.get("https://www.google.com/")
+
+print(a.json())
+
+h = a.json()
+print(h[''][''])
+"""
+
+
+
+
+
+
+import requests as rq
+import sys
+
+
+while True:
+
+  que = input("question = ")
+
+  if que != "end chat":
+    GPT =  "https://one-api.ir/chatgpt/?token=843530:657815b9d8c49&action=gpt3.5-turbo&q=" + que
+    a = rq.get(GPT)
+    j = a.json()
+    j = j['result'][0]
+
+    print('robot : ' + j)
+  else :
+    sys.exit()
